2a7f540ddc02fe025280f1b2f06710177b506aef
aimxn <aimxn@aimxnhkm.com>
2020-11-17 15:55:40 +0000

    My Second Commit

---
+++ userinput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*This is my second program in C */
+	int age;
+	printf("Hi, How old are you? >");
+	scanf("%d", &age);
+	printf("You are %d years old\n", age);
+
+	return (0);
+}
